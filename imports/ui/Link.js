c1cadcc9accf05192147727351b8b62f85f0fa12
Kartikeya Tewari <KartikeyaTewari1991@gmail.com>
2019-06-19 23:36:23 +0530

    initial commit

---
+++ imports/ui/Link.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+import LinksList from './LinksList';
+import PrivateHeader from './PrivateHeader';
+import AddLink from './AddLink';
+import LinksListFilters from './LinksListFilters';
+
+export default ()=>{
+  return (
+    <div>
+
+      <PrivateHeader title="Short Lnk"/>
+      <div className="page-content"> 
+        <LinksListFilters/>
+        <AddLink/>
+        <LinksList/>
+      </div>
+      
+
+    </div>
+  );
+}
---
+++ imports/ui/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export default ()=> {
+
+    return (
+        <div className="boxed-view">
+            <div className="boxed-view__box">
+                <h1>Page not Found</h1>
+                <p>Unable to find that page</p>
+                <Link to="/" className="button button--link"> Head Home </Link>
+            </div>
+        </div>
+    );
+
+}
